57b618f4b7e8d91aca0a1d86c094a731558a2a7e
wachirawit autamakorn <wachirawitautamakorn@gmail.com>
2024-06-12 14:15:28 +0700

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { NavController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  constructor (private navCtrl: NavController) { }
 
+  goAbout() {
+   this.navCtrl.navigateForward('/tabs/tab2');
+}
+
+goContact() {
+  this.navCtrl.navigateForward('/tabs/tab3');
+}
 }
